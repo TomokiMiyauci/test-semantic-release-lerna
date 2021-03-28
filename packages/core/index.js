523c692b4c41b8fffd0ecaeb6318a2f836683e53
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-03-29 01:24:08 +0900

    :bug: (core) aaa

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.hoge = void 0;
-var hoge = function () { return console.log(1233); };
+var hoge = function () { return console.log(233); };
 exports.hoge = hoge;
